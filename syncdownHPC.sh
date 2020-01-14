bc29ada924548d5229e7349378ac5b92b615397e
Nicholas Siemons <nicholas9182@gmail.com>
2020-01-14 09:08:47 +0000

    QCC update

--- syncdownHPC.sh
+++ syncdownHPC.sh
@@ -2,25 +2,24 @@ source ~/.zshrc
 
 cd ./0001/
 dsd
-scp HPC:$H/\*log .
-scp HPC:$H/\*pdb .
-scp HPC:$H/\*gjf .
+scp HPC:$H/\*log . > /dev/null 2>&1
+scp HPC:$H/\*pdb . > /dev/null 2>&1
+scp HPC:$H/\*txt . > /dev/null 2>&1
 
 cd ../0002/
 dsd
-scp HPC:$H/\*log .
-scp HPC:$H/\*pdb .
-scp HPC:$H/\*gjf .
+scp HPC:$H/\*log . > /dev/null 2>&1
+scp HPC:$H/\*pdb . > /dev/null 2>&1
+scp HPC:$H/\*txt . > /dev/null 2>&1
 
 cd ../0003/
 dsd
-scp HPC:$H/\*log .
-scp HPC:$H/\*pdb .
-scp HPC:$H/\*gjf .
+scp HPC:$H/\*log . > /dev/null 2>&1
+scp HPC:$H/\*pdb . > /dev/null 2>&1
+scp HPC:$H/\*txt . > /dev/null 2>&1
 
 cd ../File_Lib/
 dsd
-scp HPC:$H/\*log .
-scp HPC:$H/\*pdb .
-scp HPC:$H/\*gjf .
-
+scp HPC:$H/\*log . > /dev/null 2>&1
+scp HPC:$H/\*pdb . > /dev/null 2>&1
+scp HPC:$H/\*txt . > /dev/null 2>&1
